c1b048c4d4ae3781e980e0bc227f8be78252dbf5
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-11-28 14:26:29 -0300

    adicionando tabuada no select

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 let numEsc = document.querySelector("#txt-esc")
 
-const table = document.querySelector("select option")
+const table = document.querySelector("select")
 const error = document.querySelector("#error")
+
+const form = document.querySelector("form")
+form.addEventListener("submit", event => {
+    event.preventDefault()
+})
 
 const btnGenerateTable = document.querySelector("#btn-submit")
 btnGenerateTable.addEventListener("click", () => {
     if (numEsc.value === "") {
         error.innerHTML = "<font color='red'>Insira um número da tabuada</font>"
+        clearTableAndError()
     } else {
         let formatNumEsc = Number(numEsc.value)
+        clearTableAndError()
         for (let count = 0; count <= 10; count++) {
-            table.innerHTML = `${formatNumEsc} X ${count} = ${formatNumEsc * count}`
+            let itemTable = document.createElement("option")
+            itemTable.text = `${formatNumEsc} X ${count} = ${formatNumEsc * count}`
+            table.appendChild(itemTable)
         }
     }
 })
 
 const btnClear = document.querySelector("#btn-clear")
 btnClear.addEventListener("click", () => {
-    error.innerHTML = ""
+    clearTableAndError()
     table.innerHTML = "Digite um número acima"
     numEsc.value = ""
     numEsc.focus()
 })
+
+function clearTableAndError() {
+    error.innerHTML = ""
+    table.innerHTML = ""
+}
