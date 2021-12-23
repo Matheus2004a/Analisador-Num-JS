52b95aeebea6d23fea5a4a6f75870de35a54a625
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-12-22 21:41:19 -0300

    Adicionando valores no array ao clicar no botão adicionar

--- main.js
+++ main.js
@@ -1,38 +1,105 @@
-let numEsc = document.querySelector("#txt-esc")
-
-const table = document.querySelector("select")
-const error = document.querySelector("#error")
+let numEsc = document.querySelector("#num-esc")
+let list = document.querySelector("select")
+const resultList = document.querySelector(".content-resp")
 
 const form = document.querySelector("form")
 form.addEventListener("submit", event => {
     event.preventDefault()
 })
 
-const btnGenerateTable = document.querySelector("#btn-submit")
-btnGenerateTable.addEventListener("click", () => {
-    if (numEsc.value === "") {
-        error.innerHTML = "<font color='red'>Insira um número da tabuada</font>"
-        table.innerHTML = ""
+let listNumbers = []
+
+const btnGenerateNumbers = document.querySelector("#btn-submit")
+btnGenerateNumbers.addEventListener("click", calcNumbers)
+
+function isNumber(number) {
+    if (Number(number) >= 1 && Number(number) <= 100) {
+        return true
     } else {
-        let formatNumEsc = Number(numEsc.value)
-        clearTableAndError()
-        for (let count = 0; count <= 10; count++) {
-            let itemTable = document.createElement("option")
-            itemTable.text = `${formatNumEsc} X ${count} = ${formatNumEsc * count}`
-            table.appendChild(itemTable)
-        }
+        error.innerHTML = ""
+
+        /* clearTableAndError() */
+        /* makeOperations() */
     }
-})
+}
+
+/* function inList(number, listVerify) {
+    if (listVerify.indexOf(Number(number)) !== -1) {
+        listNumbers.push(Number(number))
+    } else {
+        return false
+    }
+
+    makeOperations()
+     if (listVerify.indexOf(listNumbers) !== -1) {
+        listNumbers.push(Number(number))
+        return true
+    } else {
+        return false
+    } 
+} */
+
+function calcNumbers() {
+    if (isNumber(numEsc.value)) {
+        listNumbers.push(Number(numEsc.value))
+        makeOperations()
+    } else {
+        verifyErrors()
+    }
+    /* if (numEsc.value === "" || numEsc.value <= 0 || numEsc.value > 100) {
+        verifyErrors()
+    } else {
+        error.innerHTML = ""
+
+        clearTableAndError()
+        makeOperations()
+    }*/
+    console.log(listNumbers)
+}
+
+function makeOperations() {
+    for (let item in listNumbers) {
+
+        let itemTable = document.createElement("option")
+        itemTable.innerHTML = `Valor ${listNumbers[item]} adicionado`
+        list.appendChild(itemTable)
+
+        /* let sumNumbers = 0
+        let numBiggest
+
+        sumNumbers += listNumbers[item]
+
+        if (listNumbers[item] >= numBiggest) {
+            numBiggest = listNumbers[item]
+        } */
+
+        /* resultList.appendChild(respInfosItems)
+        let respInfosItems = document.createElement("p")
+        respInfosItems.innerHTML = `Somando todos os valores, temos ${sumNumbers}.` */
+    }
+}
 
 const btnClear = document.querySelector("#btn-clear")
 btnClear.addEventListener("click", () => {
-    clearTableAndError()
-    table.innerHTML = "Digite um número acima"
-    numEsc.value = ""
-    numEsc.focus()
+    if (numEsc.value === "") {
+        error.innerHTML = "<font color='#ff0000'>Adicione valores antes de finalizar</font>"
+        numEsc.focus()
+        makeOperations()
+    }
+    else {
+        clearTableAndError()
+        numEsc.value = ""
+        numEsc.focus()
+    }
 })
 
 function clearTableAndError() {
     error.innerHTML = ""
-    table.innerHTML = ""
+}
+
+const error = document.createElement("p")
+
+function verifyErrors() {
+    error.innerHTML = "<font color='#ff0000'>Valor inválido ou já encontrado na lista</font>"
+    form.appendChild(error)
 }
